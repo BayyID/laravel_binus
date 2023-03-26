cdc2c379c3228d0be38215c0f4a0f7d450578d66
BayyID <anggarabayu.id@gmail.com>
2023-03-26 13:55:32 +0700

    Commit Bayu

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Perusahaan Kursus Mobil</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Perusahaan Kursus Mobil</h1>
+		<nav>
+			<ul>
+				<li><a href="#jadwal">Jadwal Belajar</a></li>
+				<li><a href="#paket">Paket Jasa</a></li>
+				<li><a href="#trainer">Trainer</a></li>
+				<li><a href="#kendaraan">Kendaraan</a></li>
+				<li><a href="#kontak">Kontak</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<section id="jadwal">
+			<h2>Jadwal Belajar</h2>
+			<p>Kami menyediakan jadwal belajar setir mobil yang fleksibel dan dapat disesuaikan dengan kebutuhan Anda. Silakan hubungi kami untuk informasi lebih lanjut.</p>
+            <ul>
+                <li>Senin - Sabtu: 08.00 - 20.00</li>
+                <li>Minggu: 09.00 - 16.00</li>
+            </ul>
+        </section>
+		<section id="paket">
+			<h2>Paket Jasa</h2>
+			<ul>
+				<li>Paket Basic: 10 jam belajar setir mobil, Rp 2.000.000</li>
+				<li>Paket Intermediate: 20 jam belajar setir mobil, Rp 3.500.000</li>
+				<li>Paket Advanced: 30 jam belajar setir mobil, Rp 5.000.000</li>
+			</ul>
+		</section>
+
+		<section id="trainer">
+			<h2>Trainer</h2>
+			<ul>
+				<li>Bambang Sutanto</li>
+				<li>Irwan Setiawan</li>
+				<li>Yuni Astuti</li>
+			</ul>
+			<p>Semua trainer kami adalah profesional dan berpengalaman dalam mengajar setir mobil.</p>
+		</section>
+
+		<section id="kendaraan">
+			<h2>Kendaraan</h2>
+			<p>Kami menyediakan berbagai jenis mobil untuk keperluan belajar setir mobil, baik manual maupun matic. Jumlah mobil yang disediakan dapat disesuaikan dengan kebutuhan Anda.</p>
+		</section>
+
+		<section id="kontak">
+			<h2>Kontak</h2>
+			<ul>
+				<li>Alamat: Jl.Salemba Raya no.19 Jakarta Pusat</li>
+				<li>Telepon: 083899541446</li>
+				<li>Email: anggarabayu.id@gmail.com</li>
+			</ul>
+		</section>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 Perusahaan Kursus Mobil. All rights reserved.</p>
+	</footer>
+</body>
+</html>
